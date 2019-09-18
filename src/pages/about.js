3baf523a146b50129b641e9cb15a4b5ea0d285a8
Murilo <muriloarrudap@gmail.com>
2019-09-18 00:52:10 -0300

    Add Pages and Styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
-const IndexPage = () => {
+
+const AboutPage = () => {
   return (
     <Layout>
-      <h1>My Blog</h1>
-      <h2>Soon this will be a blog</h2>
+      <h1>About Me</h1>
+      <p>
+        Hello, my name is Murilo and i'm a fullstack developer living in Brazil.
+      </p>
       <p>
         Need a talk? <Link to="/contact">Contact Me.</Link>
       </p>
@@ -13,4 +16,4 @@ const IndexPage = () => {
   );
 };
 
-export default IndexPage;
+export default AboutPage;
---
+++ src/pages/contact.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const ContactPage = () => {
+  return (
+    <Layout>
+      <h1>Contact</h1>
+      <p>Want hire me? please contact me at:</p>
+      <p>
+        <a
+          href="http://twitter.com/passocabr"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </p>
+    </Layout>
+  );
+};
+
+export default ContactPage;
